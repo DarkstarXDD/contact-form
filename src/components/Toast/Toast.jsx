96e13c6403ed6ee0ae8ea19156913d21cec3866a
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-10-04 09:49:00 +0530

    refactor: fixed position for toast

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -4,7 +4,7 @@ export default function Toast({ icon, title, message }) {
   return (
     <div className={styles.toastWrapper}>
       <div className={styles.toast}>
-        <img src={icon} alt="" />
+        <img className={styles.toastIcon} src={icon} alt="" />
         <p className={styles.toastTitle}>{title}</p>
         <p className={styles.toastText}>{message}</p>
       </div>
