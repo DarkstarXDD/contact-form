d4d4f860d6cd3b1acad52b502f6850b60883bb80
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-10-04 12:10:29 +0530

    fix: aria-live not being announced for toast

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -4,11 +4,11 @@ export default function Toast({ isFormValid, icon, title, message }) {
   const className = isFormValid ? styles.toastWrapper : "visually-hidden"
 
   return (
-    <div aria-live="polite" className={className}>
+    <div aria-live="assertive" className={className}>
       <div className={styles.toast}>
         <img className={styles.toastIcon} src={icon} alt="" />
-        <p className={styles.toastTitle}>{title}</p>
-        <p className={styles.toastText}>{message}</p>
+        <p className={styles.toastTitle}>{isFormValid ? title : ""}</p>
+        <p className={styles.toastText}>{isFormValid ? message : ""}</p>
       </div>
     </div>
   )
